cb8366ea218c192c36c43a043a6a856f9652d060
akshat06 <akshat.pant06@gmail.com>
2021-08-18 23:31:56 +0530

    commit done..

--- main.py
+++ main.py
@@ -94,8 +94,9 @@ def trainRouteClient():
 
 port = int(os.getenv("PORT",5000))
 if __name__ == "__main__":
-    host = '0.0.0.0'
-    #port = 5000
-    httpd = simple_server.make_server(host, port, app)
-    # print("Serving on %s %d" % (host, port))
-    httpd.serve_forever()
+    app.run()
+    # host = '0.0.0.0'
+    # #port = 5000
+    # httpd = simple_server.make_server(host, port, app)
+    # # print("Serving on %s %d" % (host, port))
+    # httpd.serve_forever()
